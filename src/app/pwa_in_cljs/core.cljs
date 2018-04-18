c1953b82f98bd1496a4117f8b591293cc785964e
OHTA Shogo <athos0220@gmail.com>
2018-04-18 15:04:13 +0900

    Load/save selected cities from/to localStorage

--- src/app/pwa_in_cljs/core.cljs
+++ src/app/pwa_in_cljs/core.cljs
@@ -1,16 +1,21 @@
 (ns pwa-in-cljs.core
   (:require [goog.dom :as dom]
             [goog.events :as events]
+            [kitchen-async.promise :as p]
+            [pwa-in-cljs.handler :as handler]
             [pwa-in-cljs.views :as views]
             [reagent.core :as r]))
 
 (enable-console-print!)
 
-(defn mount []
+(defn main []
   (println "mounting ...")
-  (r/render [views/app] (dom/getElement "app")))
+  (r/render [views/app] (dom/getElement "app"))
+  (p/let [cities (handler/load-selected-cities)]
+    (handler/update-selected-cities cities)
+    (handler/update-forecasts)))
 
-(events/listen js/window "load" mount)
+(events/listen js/window "load" main)
 
 (defn on-js-reload []
-  (mount))
+  (main))
--- src/app/pwa_in_cljs/handler.cljs
+++ src/app/pwa_in_cljs/handler.cljs
@@ -1,10 +1,23 @@
 (ns pwa-in-cljs.handler
-  (:require [kitchen-async.promise :as p]
+  (:require [cljs.reader :as reader]
+            [kitchen-async.promise :as p]
             [pwa-in-cljs.api :as api]
             [pwa-in-cljs.data :as data]))
 
 (defn toggle-add-dialog [shown?]
   (swap! data/app-state assoc :dialog-shown? shown?))
+
+(defn save-selected-cities []
+  (let [{:keys [selected-cities]} @data/app-state
+        serialized (pr-str selected-cities)]
+    (js/localStorage.setItem "selected-cities" serialized)))
+
+(defn load-selected-cities []
+  ;; TODO: should not use localStorage
+  (p/promise [resolve]
+    (let [serialized (js/localStorage.getItem "selected-cities")]
+      (resolve (reader/read-string serialized)))))
+
 (defn update-forecast-card [key card]
   (swap! data/app-state assoc-in [:visible-cards key] card))
 
@@ -17,8 +30,12 @@
   (let [{:keys [visible-cards]} @data/app-state]
     (p/do (fetch-forecast key)
           (when-not (contains? visible-cards key)
-            (swap! data/app-state update :selected-cities conj key))
+            (swap! data/app-state update :selected-cities conj key)
+            (save-selected-cities))
           (toggle-add-dialog false))))
+
+(defn update-selected-cities [cities]
+  (swap! data/app-state assoc :selected-cities (vec cities)))
 
 (defn update-forecasts []
   (let [{:keys [selected-cities]} @data/app-state]
