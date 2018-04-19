2a77c2a90f773ae119566ef05a78b2da6caadd4b
OHTA Shogo <athos0220@gmail.com>
2018-04-19 12:18:38 +0900

    Cache results for API calls

--- src/app/pwa_in_cljs/api.cljs
+++ src/app/pwa_in_cljs/api.cljs
@@ -4,7 +4,11 @@
 (defn fetch-forecast [key label]
   (let [statement (str "select * from weather.forecast where woeid=" key)
         url (str "https://query.yahooapis.com/v1/public/yql?format=json&q=" statement)]
-    (p/let [json (p/-> (js/fetch url) .json)]
+    (p/let [res (if (exists? (.-caches js/window))
+                  (p/let [res (js/caches.match url)]
+                    (or res (js/fetch url)))
+                  (js/fetch url))
+            json (.json res)]
       (let [results (-> (.. json -query -results)
                         (js->clj :keywordize-keys true))]
         (assoc results
--- src/worker/pwa_in_cljs/service_worker.cljs
+++ src/worker/pwa_in_cljs/service_worker.cljs
@@ -1,10 +1,11 @@
 (ns pwa-in-cljs.service-worker
-  (:require [kitchen-async.promise :as p]
+  (:require [clojure.string :as str]
+            [kitchen-async.promise :as p]
             [goog.events :as events]))
 
 (enable-console-print!)
 
-(def CACHE_NAME "weatherPWA-step-5-2")
+(def CACHE_NAME "weatherPWA-final-1")
 (def files-to-cache
   #js ["/"
        "/index.html"
@@ -24,6 +25,9 @@
        "/images/thunderstorm.png"
        "/images/wind.png"])
 
+(def DATA_CACHE_NAME "weatherData-v1")
+(def data-url "https://query.yahooapis.com/v1/public/yql")
+
 (defn install [event]
   (println "[ServiceWorker] Install")
   (->> (p/let [cache (js/caches.open CACHE_NAME)]
@@ -38,7 +42,7 @@
   (->> (p/let [keys (js/caches.keys)]
          (->> keys
               (map (fn [key]
-                     (when-not (= key CACHE_NAME)
+                     (when-not (#{CACHE_NAME DATA_CACHE_NAME} key)
                        (println "[ServiceWorker] Removing old cache" key)
                        (.delete js/caches key))))
               p/all))
@@ -47,9 +51,15 @@
 (.addEventListener js/self "activate" activate)
 
 (defn fetch [event]
-  (println "[ServiceWorker] Fetch" (.. event -request -url))
-  (->> (p/let [res (js/caches.match (.-request event))]
-         (or res (js/fetch (.-request event))))
-       (.respondWith event)))
+  (let [req (.-request event), url (.-url req)]
+    (println "[ServiceWorker] Fetch" url)
+    (->> (if (str/starts-with? url data-url)
+           (p/let [cache (js/caches.open DATA_CACHE_NAME)
+                   res (js/fetch req)]
+             (do (.put cache url (.clone res))
+                 res))
+           (p/let [res (js/caches.match req)]
+             (or res (js/fetch req))))
+         (.respondWith event))))
 
 (.addEventListener js/self "fetch" fetch)
